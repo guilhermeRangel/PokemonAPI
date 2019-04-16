08c33494c66b80dc4f98187b53203cad37bb58bc
Vitor Demenighi <vitor_m2@hotmail.com>
2019-04-16 17:26:24 -0300

    ajustes details card

--- nanoAPI/nanoAPI/Controller/CardDetails.swift
+++ nanoAPI/nanoAPI/Controller/CardDetails.swift
@@ -17,14 +17,15 @@ class CardDetails: UITableViewController{
     
     @IBOutlet weak var cardName: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var typeLabel2: UILabel!
     @IBOutlet weak var hpLabel: UILabel!
     @IBOutlet weak var weaknessLabel: UILabel!
     
     
-    @IBOutlet weak var abilityCell: UITableViewCell!
     @IBOutlet weak var abilityTitleLabel: UILabel!
     @IBOutlet weak var abilityDescription: UILabel!
     @IBOutlet weak var abilityTypeLabel: UILabel!
+    @IBOutlet weak var damageLabelStatic: UILabel!
     
     
     @IBOutlet weak var attack1Cell: UITableViewCell!
@@ -32,7 +33,6 @@ class CardDetails: UITableViewController{
     @IBOutlet weak var attack1_damage: UILabel!
     @IBOutlet weak var attack1_description: UILabel!
     
-    @IBOutlet var tableViewDetails: UITableView!
     
     
     @IBOutlet weak var attack2Cell: UITableViewCell!
@@ -41,7 +41,10 @@ class CardDetails: UITableViewController{
     @IBOutlet weak var attack2_damage: UILabel!
     
     
-    
+    @IBOutlet weak var attack3_name: UILabel!
+    @IBOutlet weak var attack3_description: UILabel!
+    @IBOutlet weak var attack3_damage: UILabel!
+    @IBOutlet weak var attack3Cell: UITableViewCell!
     
     override func viewWillAppear(_ animated: Bool) {
         
@@ -53,8 +56,21 @@ class CardDetails: UITableViewController{
         cardImageView.image = UIImage(data: data!)
         
         cardName.text = card?.name
+        
+        
+        
+        
+        
         hpLabel.text = "HP  \(card?.hp ?? "")"
-        typeLabel.text = card?.types![0]
+        
+        if card?.types?.count == 2 {
+            typeLabel.text = card?.types![0]
+            typeLabel2.text = card?.types![1]
+        } else {
+            typeLabel.text = card?.types![0]
+            typeLabel2.isHidden =  true
+        }
+        
         weaknessLabel.text = card?.weaknesses![0].type
         
         
@@ -64,36 +80,77 @@ class CardDetails: UITableViewController{
         
         
         if card?.ability != nil {
-            abilityCell.isHidden = false
             
+            damageLabelStatic.isHidden = true
+            attack1_name.isHidden = true
+            attack1_damage.isHidden = true
+            attack1_description.isHidden = true
             abilityTypeLabel.text = card?.ability?.type
             abilityTitleLabel.text = card?.ability?.name
             abilityDescription.text = card?.ability?.text
             
+            
+            if card?.attacks != nil && card?.attacks?.count == 2 {
+                attack1Cell.isHidden = false
+                attack2Cell.isHidden = false
+                attack3Cell.isHidden = false
+                
+                attack2_name.text = card?.attacks![0].name
+                attack2_damage.text = card?.attacks![0].damage
+                attack2_description.text = card?.attacks![0].text
+                
+                attack3_name.text = card?.attacks![1].name
+                attack3_damage.text = card?.attacks![1].damage
+                attack3_description.text = card?.attacks![1].text
+            } else {
+                attack1Cell.isHidden = false
+                attack2Cell.isHidden = false
+                attack3Cell.isHidden = true
+                
+                attack2_name.text = card?.attacks![0].name
+                attack2_damage.text = card?.attacks![0].damage
+                attack2_description.text = card?.attacks![0].text
+            }
+            
         } else {
-            abilityCell.isHidden = true
+            damageLabelStatic.isHidden = false
+            attack1_name.isHidden = false
+            attack1_damage.isHidden = false
+            attack1_description.isHidden = false
+            abilityTypeLabel.isHidden = true
+            abilityTitleLabel.isHidden = true
+            abilityDescription.isHidden = true
+            
+            
+            
+            
+            if card?.attacks != nil && card?.attacks?.count == 2 {
+                attack1Cell.isHidden = false
+                attack2Cell.isHidden = false
+                attack3Cell.isHidden = true
+                
+                attack1_name.text = card?.attacks![0].name
+                attack1_damage.text = card?.attacks![0].damage
+                attack1_description.text = card?.attacks![0].text
+                
+                attack2_name.text = card?.attacks![1].name
+                attack2_damage.text = card?.attacks![1].damage
+                attack2_description.text = card?.attacks![1].text
+            } else {
+                attack1Cell.isHidden = false
+                attack2Cell.isHidden = true
+                attack3Cell.isHidden = true
+                
+                attack1_name.text = card?.attacks![0].name
+                attack1_damage.text = card?.attacks![0].damage
+                attack1_description.text = card?.attacks![0].text
+            }
+            
+            
         }
         
         
-        if card?.attacks != nil && card?.attacks?.count == 2 {
-            attack1Cell.isHidden = false
-            attack2Cell.isHidden = false
-            
-            attack1_name.text = card?.attacks![0].name
-            attack1_damage.text = card?.attacks![0].damage
-            attack1_description.text = card?.attacks![0].text
-            
-            attack2_name.text = card?.attacks![1].name
-            attack2_damage.text = card?.attacks![1].damage
-            attack2_description.text = card?.attacks![1].text
-        } else {
-            attack1Cell.isHidden = false
-            attack2Cell.isHidden = true
-            
-            attack1_name.text = card?.attacks![0].name
-            attack1_damage.text = card?.attacks![0].damage
-            attack1_description.text = card?.attacks![0].text
-        }
+        
         
         
     }

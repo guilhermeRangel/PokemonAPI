62b5510cafe27de91bf62f1aee221176f05fa82d
Vitor Demenighi <vitor_m2@hotmail.com>
2019-04-16 16:54:37 -0300

    imageCell adicionada

---
+++ nanoAPI/nanoAPI/View/imageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  imageCollectionViewCell.swift
+//  nanoAPI
+//
+//  Created by Vitor Demenighi on 16/04/19.
+//  Copyright © 2019 Vitor Demenighi. All rights reserved.
+//
+
+import UIKit
+
+class imageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgImage: UIImageView!
+}
